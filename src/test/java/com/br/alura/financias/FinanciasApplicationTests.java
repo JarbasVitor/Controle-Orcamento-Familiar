41f6c6aa33fc3ead56bc7692651b6173a1566b6f
Jarbas Vitor <Jarbasvitor.pereira@hotmail.com>
2022-09-05 14:09:31 -0300

    Build: Project started with Spring Initialzr

---
+++ src/test/java/com/br/alura/financias/FinanciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.alura.financias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
