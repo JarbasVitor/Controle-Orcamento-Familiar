837508249f42260b25275ce1c61953e7ad7d11c5
Jarbas Vitor <Jarbasvitor.pereira@hotmail.com>
2022-09-05 23:59:27 -0300

    Feat: End Point To Return All Receitas

--- src/main/java/com/br/alura/financias/controllers/ReceitaController.java
+++ src/main/java/com/br/alura/financias/controllers/ReceitaController.java
@@ -1,19 +1,20 @@
 package com.br.alura.financias.controllers;
 
 import java.net.URI;
-import java.util.List;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.br.alura.financias.controllers.dto.ReceitaDto;
@@ -21,7 +22,7 @@ import com.br.alura.financias.controllers.form.ReceitaForm;
 import com.br.alura.financias.entities.Receita;
 import com.br.alura.financias.repositories.ReceitaRepository;
 
-@Controller
+@RestController
 @RequestMapping("/receitas")
 public class ReceitaController {
 
@@ -29,8 +30,12 @@ public class ReceitaController {
 	ReceitaRepository receitaRepository;
 	
 	@GetMapping
-	public String list() {
-		return "";
+	public Page<ReceitaDto> list(@PageableDefault(page = 0, size = 10) Pageable page) {
+		
+		Page<Receita> receitas;
+		receitas = receitaRepository.findAll(page);
+		
+		return ReceitaDto.converter(receitas);
 	}
 	
 	@PostMapping //O IDEAL É DEVOLVER CODIGO 201 NO RESPONSE ENTITY
@@ -48,6 +53,6 @@ public class ReceitaController {
 			return ResponseEntity.created(uri).body(new ReceitaDto(newReceita));
 		}
 		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		return ResponseEntity.status(409).build();
 	}
 }
